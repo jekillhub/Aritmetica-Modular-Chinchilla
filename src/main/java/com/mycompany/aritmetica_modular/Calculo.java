5b5e710ec86a860b4c00778e759011aa02421924
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-05-22 11:37:22 -0400

    Version 2

--- src/main/java/com/mycompany/aritmetica_modular/Calculo.java
+++ src/main/java/com/mycompany/aritmetica_modular/Calculo.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.aritmetica_modular;
 
-/**
- *
- * @author Jekill
- */
 public class Calculo {
+
+    public Calculo() {
+    }
+    
+    public static int[][] sumaMatriz(int[][] matriz){
+        for (int i=0; i< matriz.length;i++){
+            for (int j=0; j<matriz.length;i++){
+                matriz[i][j] = i+j;
+            }
+        }
+        return matriz;
+    }
+    
+    public static int[][] mulMatriz(int[][] matriz){
+        for (int i=0; i< matriz.length;i++){
+            for (int j=0; j<matriz.length;i++){
+                matriz[i][j] = i*j;
+            }
+        }
+        return matriz;
+    } 
     
 }
