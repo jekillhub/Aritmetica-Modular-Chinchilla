fe53e4f5ab8df1d7238d1d5481c821b1fa2c76e3
diegosauriox <d.vega03@ufromail.cl>
2018-05-30 21:41:24 -0400

    version final

--- src/main/java/com/mycompany/aritmetica_modular/Calculo.java
+++ src/main/java/com/mycompany/aritmetica_modular/Calculo.java
@@ -21,13 +21,26 @@ public class Calculo {
     
 
     public static int[][] mulMatriz(int[][] matriz) {
+        int llenado;
+        int base = matriz.length;
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; i++) {
-                int multiplicacion = i * j;
-                matriz[i][j] = multiplicacion % matriz.length;
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                llenado = i * j;
+
+                llenado = revisar(llenado, base);
+
+                matriz[i][j] = llenado;
             }
         }
         return matriz;
     }
 
+    public static int revisar(int n, int base) {
+        while (n >= base) {
+
+            n = n - base;
+        }
+        return n;
+    }
 }
--- src/test/java/CalculoTest.java
+++ src/test/java/CalculoTest.java
@@ -2,14 +2,20 @@ import com.mycompany.aritmetica_modular.Calculo;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import com.mycompany.aritmetica_modular.IO;
+import org.junit.Ignore;
 
 public class CalculoTest {
     
-    @Test
+    @Ignore
     public void sumaMatrizTest(){
-        int[][] matrizEsperada ={{0,1,2},{1,2,3},{2,3,0}};
+        int[][] matrizEsperada ={{0,1,2},{1,2,0},{2,0,1}};
         int[][] matrizIngresada = new int[3][3];
         assertArrayEquals(matrizEsperada, Calculo.sumaMatriz(matrizIngresada));
     }
-    
+    @Test
+    public void multiplicarTest(){
+        int[][] matrizEsperada ={{0,0,0},{0,1,2},{0,2,1}};
+        int[][] matrizIngresada = new int[3][3];
+        assertArrayEquals(matrizEsperada, Calculo.mulMatriz(matrizIngresada));
+    }
 }
